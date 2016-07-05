02670617e0946cc7270767a74a33df8dd8560128
Irina <irinargerenrot@gmail.com>
2016-07-05 23:42:34 +0000

    Ajar.v1

--- Ajar.io/app.js
+++ Ajar.io/app.js
@@ -1,15 +1,16 @@
+var arrayOfFoodObjects = [];
+    for (var i = 0; i < 50; i++) {
+        arrayOfFoodObjects.push(createFoodObject());
+    }
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var x = 250;
 var y = 250;
-
-//draw circle
-function drawUserCell() {
-    context.beginPath();
-    context.arc(x, y, 40, 0, 2*3.141592655);
-    context.fillStyle = "cyan";
-    context.fill();
-}
+var mouseX = 250;
+var mouseY = 250;
+var velocity = 5;
+var sRadius = 10;
+var bRadius = 40;
 
 function clearCanvas() {
     context.beginPath();
@@ -18,21 +19,81 @@ function clearCanvas() {
     context.fill();
 }
 
-function mouseMoved(mouse) {
-    x = mouse.clientX;
-    y = mouse.clientY;
-    clearCanvas();
-    drawUserCell();
+function calculatePosition() {
+    // For the x-axis
+    if (mouseX > x) {
+        x += velocity;  // mouse is to the right of the ball
+    } else {
+        x -= velocity;  // mouse is to the left of the ball
+    }
+    
+    // For the y-axis
+    if (mouseY > y) {
+        y += velocity;  // mouse is under ball
+    } else {
+        y -= velocity;  // mouse is above ball
+    }
 }
-
 
 function drawScreen() {
-    clearCanvas();
-    drawUserCell();
-    setTimeout(drawScreen, 1000/60);
+    if (arrayOfFoodObjects.length == 0) {
+        gameOver();
+    } else {
+            clearCanvas();
+        calculatePosition();
+        drawBigCircle();
+        for (var n = 0; n < arrayOfFoodObjects.length; n++) {
+            
+            var sCircle = arrayOfFoodObjects[n];
+            
+            if (bRadius + sRadius> Math.sqrt(Math.pow(Math.abs(x - sCircle.position.x), 2) + Math.pow(Math.abs(y - sCircle.position.y), 2))) {
+                arrayOfFoodObjects.splice(n, 1);
+                bRadius += 3;
+            }
+            context.beginPath();
+            context.arc(sCircle.position.x, sCircle.position.y, sRadius, 0, 2*3.141592655);
+            context.fillStyle = sCircle.color;
+            context.fill();
+        }
+    
+        setTimeout(drawScreen, 1000/60);
+    }
 }
+
+function mouseMoved(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+function createFoodObject() {
+    return {
+        color: "Lime",
+        position: {
+            x: Math.random() * 500,
+            y: Math.random() * 500
+        }
+    };
+}
+
+function drawBigCircle() {
+    context.beginPath();
+    context.arc(x, y, bRadius, 0, 2*3.141592655);
+    context.fillStyle = "cyan";
+    context.fill();
+}
+
+
+
 //so it follows mouse
 canvas.addEventListener("mousemove", mouseMoved);
 
 //so you start with circle in middle--not very necessary
-drawUserCell();
+drawScreen();
+
+function gameOver() {
+    context.beginPath();
+    context.rect(0, 0, 500, 500);
+    context.fillStyle = "navy";
+    context.fill();
+    
+}
--- platformer/game.js
+++ platformer/game.js
@@ -5,16 +5,16 @@ var GROUND_SPRITE_WIDTH = 50;
 var GROUND_SPRITE_HEIGHT = 50;
 var numGroundSprites;
 var obstacleSprites;
-var scoreBoost;
+// var scoreBoost;
 
 var player;
 
 var isGameOver;
-var score;
+// var score;
 
 function setup() {
-    isGameOver = false;
-    score = 0;
+//     isGameOver = false;
+//     score = 0;
     
     createCanvas(400, 300);
     background(150, 200, 250);
@@ -26,82 +26,71 @@ function setup() {
         groundSprites.add(groundSprite);
     }
     player = createSprite(100, height - 75, 50, 50);
-    
     obstacleSprites = new Group();
-    scoreBoost = new Group();
+//     scoreBoost = new Group();
 }
 
 function draw() {
-    
     if (isGameOver) {
         background(0);
         fill(255);
         textAlign(CENTER);
         text("Game Over! Click anywhere to restart", camera.position.x, camera.position.y);
-        text("Your score was: " + score, camera.position.x, camera.position.y - 20);
     } else {
     
         background(150, 200, 250);
         player.velocity.y = player.velocity.y + GRAVITY;
-        
         if (groundSprites.overlap(player)) {
             player.velocity.y = 0;
-            player.position.y = (height - 50) - (player.height/2);
+            player.position.y = (height-50) - (player.height/2);
         }
-        
+    
         if (keyDown(UP_ARROW)) {
             player.velocity.y = JUMP;
         }
         
-        player.position.x = player.position.x + 5;
+        player.position.x += 5;
         camera.position.x = player.position.x + (width / 4);
         
         var firstGroundSprite = groundSprites[0];
-        if (firstGroundSprite.position.x <= camera.position.x - (width / 2 + firstGroundSprite.width/2)) {
+        if (firstGroundSprite.position.x <= camera.position.x - (width/2 + firstGroundSprite.width/2)) {
             groundSprites.remove(firstGroundSprite);
             firstGroundSprite.position.x = firstGroundSprite.position.x + numGroundSprites*firstGroundSprite.width;
             groundSprites.add(firstGroundSprite);
         }
-        
-        if (random() > 0.95) {
-            var obstacle = createSprite(camera.position.x + width, random(0, (height -  50) - 15), 30, 30);
+    
+        if (random() > .95) {
+            var obstacle = createSprite(camera.position.x + width, (height-50) - 15, 30, 30);
             obstacleSprites.add(obstacle);
         }
-        
         var firstObstacle = obstacleSprites[0];
         if (obstacleSprites.length > 0 && firstObstacle.position.x <= camera.position.x - (width/2 + firstObstacle.width/2)) {
             removeSprite(firstObstacle);
         }
         
-        var firstScoreBoost = scoreBoost[0];
-        if (firstScoreBoost.length > 0 && firstScoreBoost.position.x <= camera.position.x - (width/2 + firstScoreBoost.width/2)) {
-            removeSprite(firstScoreBoost);
-        }
-        
         obstacleSprites.overlap(player, endGame);
-        
         drawSprites();
-        score++;
-        textAlign(CENTER);
-        text(score, camera.position.x, 10);
     }
+    
 }
 
 function endGame() {
     isGameOver = true;
 }
- 
+
 function mouseClicked() {
-     
-    for (var n = 0; n < numGroundSprites; n++) {
-        var groundSprite = groundSprites[n];
-        groundSprite.position.x = n*50;
+    if (isGameOver) {
+
+        for (var n = 0; n < numGroundSprites; n++) {
+            var groundSprite = groundSprites[n];
+            groundSprite.position.x = n*50;
+        }
+
+        player.position.x = 100;
+        player.position.y = height-75;
+    
+        obstacleSprites.removeSprites();
+    
+        isGameOver = false;
     }
-    
-    player.position.x = 100;
-    player.position.y = height - 75;
-    
-    obstacleSprites.removeSprites();
-    score = 0;
-    isGameOver = false;
 }
