cfbe8b4abdfa69ee532afc0d8e99d7087cf0ae7d
Irina <irinargerenrot@gmail.com>
2016-07-08 23:33:47 +0000

    added cloud

--- summer/main.js
+++ summer/main.js
@@ -3,6 +3,12 @@ var person;
 var personImage;
 var personInitialY = 350;
 var personChangeY = 0;
+var xB = 200;
+var yB = 100;
+var xL = 170;
+var yL = 104;
+var yR = 104;
+var xR = 230;
 
 function setup() {
     createCanvas(500, 500);
@@ -21,6 +27,8 @@ function draw() {
     noStroke();
     ellipse(240, 75, sunRadius, sunRadius);
     
+    drawCloud();
+    
     //sand
     fill(255, 255, 102);
     noStroke();
@@ -31,10 +39,18 @@ function draw() {
     noStroke();
     rect(0, 375, canvas.width, canvas.height - 475);
     
-    //increasing sunRadius
+    //text
+    fill(128, 0, 0);
+    textSize(15);
+    textStyle(BOLD);
+    text("Press Up to Watch Gary Melt", 10, 450);
+    text("Press Down to Revive Him", 10, 470);
+    text("The Cloud Helps Gary!", 10, 490)
+    
+    //changing sunRadius & melting person
     if (keyDown(UP_ARROW) && personImage.height >= 0) {
         sunRadius += 5;
-        if (personImage.height >= 0) {
+        if (personImage.height >= 0 && sunRadius >= 70) {
             personImage.width += 2;
             personImage.height -= 2;
             personChangeY += 1;
@@ -49,13 +65,33 @@ function draw() {
         personChangeY -= 1;
     }
     person.position.y = personInitialY + personChangeY;
-
+    
     drawSprites();
+    
     //summerperson shadow
-    // fill(0);
-    // ellipse(355, 460, personImage.width - 100, 10);
+    fill(0);
+    ellipse(355, 460, personImage.width - 100, 10);
 }
 
 function preload() {
     personImage = loadImage("summerperson.png");
+}
+
+function drawCloud() {
+    fill(255);
+    ellipse(xB, yB, 70, 70);
+    ellipse(xL, yL, 50, 50);
+    ellipse(xR, yR, 50, 50);
+    
+    if (keyDown(LEFT_ARROW) && xB >= 0) {
+        xB -= 4;
+        xL -= 4;
+        xR -= 4;
+    }
+    
+    if (keyDown(RIGHT_ARROW) && xB <= canvas.width) {
+        xB += 4;
+        xL += 4;
+        xR += 4;
+    }
 }
